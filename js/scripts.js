8a046ecdcacd172ba4c38f7639c3cb98222ed18d
ldionisio <ldionisiomoles@gmail.com>
2020-07-28 13:22:33 +0200

    NPM Scripts Part 2

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $('#mycarousel').carousel({interval: 2000});
+    $('#carouselButton').click(function(){
+
+        if($('#carouselButton').children('span').hasClass('fa-pause')){
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if($('#carouselButton').children('span').hasClass('fa-play')){
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+
+    });
+    
+    $('#showTable').click(function(){
+        $('#reserveModal').modal('show');
+    });
+    
+    $('#showLogin').click(function(){
+        $('#loginModal').modal('show');
+    });
+    
+ });
+
+ /*
+ <script>
+     $(document).ready(function(){
+         $('[data-toggle="tooltip"]').tooltip();
+     });
+ </script>
+ */
